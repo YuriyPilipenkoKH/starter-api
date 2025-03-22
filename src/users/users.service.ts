3b76cd1c6ead6adc6fd8f33ad12faa2244f01164
Yuriy Pilipenko <yurik2061@gmail.com>
2025-03-22 15:12:18 +0200

    delete method added

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -74,5 +74,13 @@ export class UsersService {
     })
 
     return this.findOne(id)
-}
+  }
+
+  delete(id: number) {
+    const removedUser = this.findOne(id)
+
+    this.users = this.users.filter(user => user.id !== id)
+
+    return removedUser
+  }
 }

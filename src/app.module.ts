d7bd8956668b30a2a4db5531b8e08fa68ef04c33
Yuriy Pilipenko <yurik2061@gmail.com>
2025-03-23 19:38:54 +0200

    database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 
  @Module({
-  imports: [UsersModule, ConfigModule.forRoot()],
+  imports: [UsersModule, ConfigModule.forRoot(), DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })

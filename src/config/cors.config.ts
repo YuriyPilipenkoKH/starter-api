b956e4bca9c392fbf1d40f12ca548d674ec05a74
Yuriy Pilipenko <yurik2061@gmail.com>
2025-03-25 08:28:16 +0200

    enabled cors

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,11 +1,14 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
+const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN
+
 export const corsConfig: CorsOptions = {
   origin: [
-    'http://localhost:3000',
-    'https://your-frontend-domain.com',
+    CLIENT_ORIGIN,
+    'http://localhost:5173', 
+    'http://localhost:3000', 
   ],
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', "Cookie"],
   credentials: true,
 };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { corsConfig } from './config/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(corsConfig); 
   app.setGlobalPrefix('api')
   void app.listen(process.env.PORT ?? 3000);
 }
